679ac88c419a74748d09fcb82985a1b2afaf2cf0
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2023-12-03 23:33:26 +0100

    Day 3 part 2

--- src/2023/3.ts
+++ src/2023/3.ts
@@ -1,16 +1,18 @@
-import { sumArrayValues } from './support/array';
+import { multiplyArrayValues, sumArrayValues } from './support/array';
 import { withReadInput } from './support/io';
 
 withReadInput(async (input) => {
   const lines = input.split('\n').map((line) => line.trim());
 
   // { [line number]: position in line[] }
-  const capturePositions: Record<number, { from: number; to: number }[]> =
-    Object.fromEntries(
-      Array(lines.length + 2)
-        .fill(true)
-        .map((_, i) => [i - 1, []]),
-    );
+  const capturePositions: Record<
+    number,
+    { id: string; from: number; to: number; symbol: string }[]
+  > = Object.fromEntries(
+    Array(lines.length + 2)
+      .fill(true)
+      .map((_, i) => [i - 1, []]),
+  );
 
   lines.forEach((line, lineNumber) => {
     for (let i = 0; i < line.length; i += 1) {
@@ -18,14 +20,19 @@ withReadInput(async (input) => {
         continue;
       }
 
-      const inLinePositions = { from: i - 1, to: i + 1 };
+      const inLinePositions = {
+        from: i - 1,
+        to: i + 1,
+        id: `${lineNumber}-${i}`,
+        symbol: line[i],
+      };
       capturePositions[lineNumber - 1].push(inLinePositions);
       capturePositions[lineNumber].push(inLinePositions);
       capturePositions[lineNumber + 1].push(inLinePositions);
     }
   });
 
-  const numbers: number[] = [];
+  const numbers: Record<string, { numbers: number[]; symbol: string }> = {};
   lines.forEach((line, lineNumber) => {
     const matches = [...line.matchAll(/([0-9]+)/g)];
 
@@ -34,17 +41,27 @@ withReadInput(async (input) => {
       const { index: numberStart = 0 } = match;
       const numberEnd = numberStart + value.length - 1;
 
-      if (
-        capturePositions[lineNumber].some(
-          ({ from: rangeStart, to: rangeEnd }) =>
-            (numberStart >= rangeStart && numberStart <= rangeEnd) ||
-            (numberEnd >= rangeStart && numberEnd <= rangeEnd),
-        )
-      ) {
-        numbers.push(Number.parseInt(value, 10));
-      }
+      capturePositions[lineNumber].forEach(({ id, symbol, from, to }) => {
+        if (!numbers[id]) {
+          numbers[id] = { symbol, numbers: [] };
+        }
+
+        const startInRange = numberStart >= from && numberStart <= to;
+        const endInRange = numberEnd >= from && numberEnd <= to;
+
+        if (startInRange || endInRange) {
+          numbers[id].numbers.push(Number.parseInt(value, 10));
+        }
+      });
     });
   });
 
-  return sumArrayValues(numbers).toString();
+  const gearRatiosSum = sumArrayValues(
+    Object.values(numbers).map((symbolNumbers) =>
+      symbolNumbers.symbol === '*' && symbolNumbers.numbers.length === 2
+        ? multiplyArrayValues(symbolNumbers.numbers)
+        : 0,
+    ),
+  );
+  return gearRatiosSum.toString();
 });
