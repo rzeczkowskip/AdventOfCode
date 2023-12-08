aa12bd98cbb9972ec6f4dd1dc6ece0defc0eb7fa
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2023-12-08 18:39:51 +0100

    Day 8 part 2

--- src/2023/8.ts
+++ src/2023/8.ts
@@ -1,12 +1,21 @@
-import { sumArrayValues } from './support/array';
 import { withInput } from './support/io';
-import { strToInt } from './support/number';
 
-const demoInput = `LLR
+// const demoInput = `LLR
+//
+// AAA = (BBB, BBB)
+// BBB = (AAA, ZZZ)
+// ZZZ = (ZZZ, ZZZ)`;
 
-AAA = (BBB, BBB)
-BBB = (AAA, ZZZ)
-ZZZ = (ZZZ, ZZZ)`;
+const demoInput = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`;
 
 class InstructionsProvider {
   private currentIndex: number = 0;
@@ -43,20 +52,56 @@ withInput(async (input) => {
   const nodes = Object.fromEntries(
     nodesString.map((nodeString) => {
       const [, node, L, R] =
-        nodeString.match(/^([a-z]{3}) = \(([a-z]{3}), ([a-z]{3})\)$/i) || [];
+        nodeString.match(
+          /^([a-z0-9]{3}) = \(([a-z0-9]{3}), ([a-z0-9]{3})\)$/i,
+        ) || [];
 
       return [node, { L, R }];
     }),
   );
 
-  const stepProvider = new InstructionsProvider(instructions);
-  const start = 'AAA';
-  const end = 'ZZZ';
-  let current = start;
+  // const start = 'AAA';
+  // const end = 'ZZZ';
+  // let current = start;
+  //
+  // while (current !== end) {
+  //   current = nodes[current][stepProvider.next];
+  // }
+  //
 
-  while (current !== end) {
-    current = nodes[current][stepProvider.next];
+  const currentValues = Object.keys(nodes).filter((node) => node.endsWith('A'));
+  const results: number[] = [];
+  currentValues.forEach((value) => {
+    const stepProvider = new InstructionsProvider(instructions);
+
+    let current = value;
+    while (!current.endsWith('Z')) {
+      current = nodes[current][stepProvider.next];
+    }
+
+    results.push(stepProvider.steps);
+  });
+  // console.log(currentValues);
+  // while (!doesAllValuesEndWith(currentValues, 'Z')) {
+  //   const nextStep = stepProvider.next;
+  //
+  //   currentValues.forEach((current, index, array) => {
+  //     // eslint-disable-next-line no-param-reassign
+  //     array[index] = nodes[current][nextStep];
+  //   });
+  // }
+
+  const greatestCommonDivisor = (a: number, b: number): number => {
+    if (a === 0) return b;
+    return greatestCommonDivisor(b % a, a);
+  };
+
+  while (results.length > 1) {
+    const a = results.pop() as number;
+    const b = results.pop() as number;
+
+    results.push((a * b) / greatestCommonDivisor(a, b));
   }
 
-  return stepProvider.steps.toString();
+  return results[0].toString();
 }, demoInput);
